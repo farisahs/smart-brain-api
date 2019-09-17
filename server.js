5f57a7d244522094121c5663169ee06a5325a055
farisa <farisa.hafida@gmail.com>
2019-09-17 12:00:38 +0700

    changing response

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ const app = express();
 app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
-    res.send(db.users);
+    res.send('it is working!');
 })
 
 app.post('/signin', (req, res) => { signin.handleSignin(req, res, db, bcrypt) })
